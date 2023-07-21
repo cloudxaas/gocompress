5ce6c9cf625b6098075d2baac765d157e82e4542
HIQ Society <51492452+hiqsociety@users.noreply.github.com>
2023-07-21 03:21:58 -0400

    Update gzip.go

--- gzip/gzip.go
+++ gzip/gzip.go
@@ -11,31 +11,45 @@ import (
 // Compress compresses the input byte slice using gzip compression.
 func Compress(input []byte) ([]byte, error) {
 	buf := bytebufferpool.Get()
-	defer bytebufferpool.Put(buf)
+	
 
 	gz := gzip.NewWriter(buf)
 	if _, err := gz.Write(input); err != nil {
+		bytebufferpool.Put(buf)
 		return nil, err
 	}
 	if err := gz.Close(); err != nil {
+		bytebufferpool.Put(buf)
 		return nil, err
 	}
-	return buf.Bytes(), nil
+
+	// Copy the data to a new slice before returning
+	output := make([]byte, buf.Len())
+	copy(output, buf.B)
+	bytebufferpool.Put(buf)
+	return output, nil
 }
 
 // Decompress decompresses the input byte slice using gzip decompression.
 func Decompress(input []byte) ([]byte, error) {
 	buf := bytebufferpool.Get()
-	defer bytebufferpool.Put(buf)
+
 
 	gz, err := gzip.NewReader(bytes.NewReader(input))
 	if err != nil {
+		bytebufferpool.Put(buf)
 		return nil, err
 	}
 	defer gz.Close()
 
 	if _, err := io.Copy(buf, gz); err != nil {
+		bytebufferpool.Put(buf)
 		return nil, err
 	}
-	return buf.Bytes(), nil
+
+	// Copy the data to a new slice before returning
+	output := make([]byte, buf.Len())
+	copy(output, buf.B)
+	bytebufferpool.Put(buf)
+	return output, nil
 }
